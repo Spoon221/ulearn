73582745e4f9337de39c8ec87359772ccf8d2058
Spoon221 <60212126+Spoon221@users.noreply.github.com>
2022-01-21 08:33:48 +0500

    Add files via upload

---
+++ 4.Массивы/Names.csproj/HeatmapTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Names
+{
+    internal static class HeatmapTask
+    {
+        public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+        {
+            var day = FillingOffSet(30, 2);
+            var mounth = FillingOffSet(12, 1);
+            var color = new double[day.Length, mounth.Length];
+            foreach (var fertility in names)
+            {
+                if (fertility.BirthDate.Day != 1)
+                {
+                    color[fertility.BirthDate.Day - 2, fertility.BirthDate.Month - 1]++;
+                }
+            }
+            return new HeatmapData("Карта интенсивности рождаемости",
+                color, 
+                day, 
+                mounth);
+        }
+
+        public static string[] FillingOffSet(int lengthArray, int offset)
+        {
+            var array = new string[lengthArray];
+            for (var i = 0; i < lengthArray; i++)
+            { 
+                array[i] = Convert.ToString(i + offset);
+            }
+            return array;
+        }
+    }
+}
---
+++ 4.Массивы/Names.csproj/HistogramTask.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Names
+{
+    internal static class HistogramTask
+    {
+        public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+        {
+            var chooseDataBornName = new double[31];
+            foreach (var nameDay in names)
+            {
+                if (nameDay.BirthDate.Day > 1 && nameDay.Name == name)
+                {
+                    chooseDataBornName[nameDay.BirthDate.Day - 1]++;
+                }
+            }
+
+            return new HistogramData
+                (string.Format("Рождаемость людей с именем '{0}'",
+                name),
+                BirthdayCounter(),
+                chooseDataBornName);
+        }
+
+        public static string[] BirthdayCounter()
+        {
+            var signatureDays = new string[31];
+            for (var i = 0; i < signatureDays.Length; i++)
+            {
+                signatureDays[i] = Convert.ToString(i + 1);
+            }
+            return signatureDays;
+        }
+    }
+}
