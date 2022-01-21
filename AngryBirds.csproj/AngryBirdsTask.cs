e7f6215e0293d357af8f2e9bb3a2ddb16f6d5563
Spoon221 <60212126+Spoon221@users.noreply.github.com>
2022-01-21 08:01:31 +0500

    Add files via upload

---
+++ AngryBirds.csproj/AngryBirdsTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AngryBirds
+{
+	public static class AngryBirdsTask
+	{
+		const double G = 9.8f;
+
+		/// <param name="speed">Начальная скорость</param>
+		/// <param name="distance">Расстояние до цели</param>
+		/// <returns>Угол прицеливания в радианах от 0 до Pi/2</returns>
+
+		public static double FindSightAngle(double speed, double distance)
+		{
+			return Math.Asin(G * distance / (speed * speed)) / 2;
+		}
+	}
+}
