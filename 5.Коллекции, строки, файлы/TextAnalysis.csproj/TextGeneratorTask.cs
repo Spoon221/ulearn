69e10e1dada6e572fd94667344f89f17c434c63a
Spoon221 <60212126+Spoon221@users.noreply.github.com>
2022-01-21 08:36:52 +0500

    Add files via upload

---
+++ 5.Коллекции, строки, файлы/TextAnalysis.csproj/TextGeneratorTask.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace TextAnalysis
+{
+    static class TextGeneratorTask
+    {
+        public static string ContinuePhrase(Dictionary<string, string> nextWords,
+            string phraseBeginning, int wordsCount)
+        {
+            var builder = new StringBuilder();
+            builder.Append(phraseBeginning);
+            for (var i = 0; i < wordsCount; i++)
+            {
+                var phrase = builder.ToString();
+                if (GetWordCount(phrase) >= 2 && nextWords.ContainsKey(GetKey(3, phrase)))
+                    builder.Append(" " + nextWords[GetKey(3, phrase)]);
+                else if (nextWords.ContainsKey(GetKey(1, phrase)))
+                    builder.Append(" " + nextWords[GetKey(1, phrase)]);
+                else
+                    break;
+            }
+            return builder.ToString();
+        }
+
+        public static string GetKey(int index, string phrase)
+        {
+            var text = phrase.Split(' ');
+            if (index == 3)
+                return text[text.Length - 2] + " " + text[text.Length - 1];
+            else 
+                return text[text.Length - 1];
+        }
+
+        public static int GetWordCount(string phrase)
+        {
+            var words = phrase.Split(' ');
+            return words.Length;
+        }
+    }
+}
