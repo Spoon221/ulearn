99b3a1b15c089028e753f095fe4cb7354f876f00
Spoon221 <60212126+Spoon221@users.noreply.github.com>
2022-01-21 08:21:33 +0500

    Add files via upload

---
+++ 1.Ошибки/percent/BilliardsTask.cs
@@ -0,0 +1,8 @@
+﻿static double Calculate(string userInput)
+{
+	var temporaryStr = userInput.Split();
+    var sum = Convert.ToDouble(temporaryStr[0]);
+    var interestRate = Convert.ToDouble(temporaryStr[1]);
+    var termOwnMonths = Double.Parse(temporaryStr[2]);
+    return sum * Math.Pow(1 + interestRate / 1200, termOwnMonths);
+}
