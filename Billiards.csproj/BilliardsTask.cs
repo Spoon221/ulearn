93a225eedad4db3d04c110d647cdbd1245b99203
Spoon221 <60212126+Spoon221@users.noreply.github.com>
2022-01-21 08:03:54 +0500

    Add files via upload

---
+++ Billiards.csproj/BilliardsTask.cs
@@ -0,0 +1,16 @@
+﻿namespace Billiards
+{
+    public static class BilliardsTask
+    {
+        /// <summary>
+        /// Этот метод расчитывает угла отскока шарика от стены.
+        /// </summary>
+        /// <param name="directionRadians">Угол направления движения шара</param>
+        /// <param name="wallInclinationRadians">Угол</param>
+        /// <returns>Угол траектории шара оси в OX в; радианах</returns>
+        public static double BounceWall(double directionRadians, double wallInclinationRadians)
+        {
+            return 2 * wallInclinationRadians - directionRadians;
+        }
+    }
+}
